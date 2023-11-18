05ffc2f66049968f5b0de5584653d9a01df5c542
almatkz <almatrakhimzhanov@gmail.com>
2023-11-19 05:46:49 +0600

    Homework 8

--- app/src/main/java/kz/jusan/homework6/MainActivity.kt
+++ app/src/main/java/kz/jusan/homework6/MainActivity.kt
@@ -1,8 +1,11 @@
 package kz.jusan.homework6
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -11,14 +14,16 @@ import androidx.core.content.ContextCompat
 
 const val CORRECT_PIN = "1567"
 const val PIN_LENGTH = 4
+const val KEY_PIN = "pin"
 
 class MainActivity : AppCompatActivity() {
 
     private var pinText = ""
-    lateinit var  tvPin: TextView
+    private var pinTextOnSave = ""
+    lateinit var tvPin: TextView
+
     var pinTextColor: Int = Color.BLACK
     var errorColor: Int = Color.BLACK
-    var correctPinColor: Int = Color.GREEN
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,6 +33,14 @@ class MainActivity : AppCompatActivity() {
         initNumButtons()
         initBackspaceButton()
         initOkButton()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        pinText = savedInstanceState.getString(KEY_PIN) ?: ""
+        updatePinTextView()
+        checkIfPinIsCorrect()
     }
 
     private fun initColors() {
@@ -51,13 +64,15 @@ class MainActivity : AppCompatActivity() {
         val btnOk: Button = findViewById(R.id.btn_ok)
         btnOk.setOnClickListener {
             checkIfPinIsCorrect()
+            pinTextOnSave = pinText
         }
     }
 
     private fun checkIfPinIsCorrect() {
         if (pinText.equals(CORRECT_PIN)) {
             Toast.makeText(this, R.string.pin_is_correct, Toast.LENGTH_LONG).show()
-            tvPin.setTextColor(correctPinColor)
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         } else {
             tvPin.setTextColor(errorColor)
         }
@@ -113,5 +128,10 @@ class MainActivity : AppCompatActivity() {
         tvPin.setTextColor(pinTextColor)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
 
+        outState.putString(KEY_PIN, pinTextOnSave)
+        Log.e("Main Activity", "OnSaveInstance #1")
+    }
 }
---
+++ app/src/main/java/kz/jusan/homework6/SecondActivity.kt
@@ -0,0 +1,7 @@
+package kz.jusan.homework6
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity(R.layout.activity_second){
+
+}
