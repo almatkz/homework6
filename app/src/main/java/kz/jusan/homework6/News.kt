0ad5184bfa070f23fadc14118285c19fb6546b37
almatkz <almatrakhimzhanov@gmail.com>
2023-11-26 22:45:05 +0600

    Add news list fragment

---
+++ app/src/main/java/kz/jusan/homework6/News.kt
@@ -0,0 +1,7 @@
+package kz.jusan.homework6
+
+data class News(
+    val title: String,
+    val author: String,
+    val date: String
+)
---
+++ app/src/main/java/kz/jusan/homework6/NewsClickListener.kt
@@ -0,0 +1,6 @@
+package kz.jusan.homework6
+
+interface NewsClickListener {
+
+    fun onNewsClick(news: News)
+}
